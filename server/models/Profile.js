23f7ff7409681265e2035c3735e6c62ad1e1cd82
Chad Napper <contact@chadnaps.com>
2022-01-06 16:25:38 -0700

    Add schema types and structure

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -3,10 +3,16 @@ const bcrypt = require("bcryptjs");
 
 const profileSchema = new mongoose.Schema({
   user_username: {
+    type: String,
+    required: true,
   },
   first_name: {
+    type: String,
+    required: true,
   },
   last_name: {
+    type: String,
+    required: true,
   },
   availability: {
     begin: Date,
@@ -14,59 +20,112 @@ const profileSchema = new mongoose.Schema({
     days: {
       mon: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       tues: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       wed: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       thurs: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       fri: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       sat: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
       sun: {
         from: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
         to: {
+          type: Number,
+          min: 0,
+          max: 2359,
         },
       },
     },
+    required: true,
   },
   profile_picture: {
+    type: Buffer,
   },
   gender: {
+    type: String,
+    maxLength: 20,
   },
   birth_date: {
+    type: Date,
   },
   phone_number: {
+    type: String,
+    maxLength: 25,
   },
   address: {
+    type: String,
+    maxLength: 1000,
   },
   description: {
+    type: String,
+    maxLength: 3000,
   },
 });
 
